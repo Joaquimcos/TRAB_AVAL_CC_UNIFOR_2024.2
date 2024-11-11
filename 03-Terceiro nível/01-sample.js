d972da29a930787ac02d7bcba7da913aa0763fea
diegosouza001 <diegoazevedo0807@gmail.com>
2024-11-11 09:59:15 -0300

    melhorando o codigo

--- 03-Terceiro nível/01-sample.js
+++ 03-Terceiro nível/01-sample.js
@@ -1,19 +1,24 @@
 async function buscarEDetalharNave() {
     try {
         const resposta = await fetch('https://swapi.dev/api/starships/9/');
+        
+        if (!resposta.ok) {
+            throw new Error(`Erro na requisição: ${resposta.status}`);
+        }
+
         const nave = await resposta.json();
 
-        const detalhesNave = `
-        Nome: ${nave.name}
-        Modelo: ${nave.model}
-        Fabricante: ${nave.manufacturer}
-        `;
-
         console.log("Detalhes da Nave:");
-        console.log(detalhesNave);
+        console.table({
+            Nome: nave.name,
+            Modelo: nave.model,
+            Fabricante: nave.manufacturer
+        });
 
         const tamanhoTripulacao = parseInt(nave.crew);
-        if (tamanhoTripulacao > 100) {
+        if (Number.isNaN(tamanhoTripulacao)) {
+            console.log('Informação da tripulação indisponível.');
+        } else if (tamanhoTripulacao > 100) {
             console.log('Esta é uma nave grande.');
         } else {
             console.log('Esta é uma nave pequena.');
