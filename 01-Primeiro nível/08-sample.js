b300db485a4e15bf197f61dd9a109184e879113e
biiancatorres <biancatorres@edu.unifor.br>
2024-11-11 23:15:49 -0300

    Melhorando o código

--- 01-Primeiro nível/08-sample.js
+++ 01-Primeiro nível/08-sample.js
@@ -1,7 +1,8 @@
 let pessoa = { nome: "Maria", idade: 28 };
 
 function verificarMaioridade(pessoa) {
-    return pessoa.idade >= 18;
+    const maiorIdade = 18;
+    return pessoa.idade >= maiorIdade;
 }
 
 console.log(`${pessoa.nome}` + " é maior de idade? " + verificarMaioridade(pessoa));
