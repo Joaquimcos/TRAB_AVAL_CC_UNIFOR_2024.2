0df77834d4644744927269bcc8058ab7f26f88f0
biiancatorres <biancatorres@edu.unifor.br>
2024-11-12 22:54:42 -0300

    aprimorando o código

--- 01-Primeiro nível/11-sample.js
+++ 01-Primeiro nível/11-sample.js
@@ -1,85 +1,104 @@
-class z {
-    constructor(a, b, c, d, e, f, g, h) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
-        this.d = d;
-        this.e = e;
-        this.f = f;
-        this.g = g;
-        this.h = h;
+class UserProfile {
+    constructor(nomeUsuario, idade, papelUsuario, dataCriacao, dataUltLogin, status, numPontos, tentativasLogin) {
+        this.nomeUsuario = nomeUsuario; 
+        this.idade = idade; 
+        this.papelUsuario = papelUsuario; 
+        this.dataCriacao = dataCriacao; 
+        this.dataUltLogin = dataUltLogin; 
+        this.status = status; 
+        this.numPontos = numPontos; 
+        this.tentativasLogin = tentativasLogin; 
     }
 
-    x() {
-        if (this.f) {
-            console.log("Ativo");
+    verificarStatus() { 
+        if (this.status) {
+            console.log(`${this.nomeUsuario} é um usuário Ativo`);
         } else {
-            console.log("Inativo");
+            console.log(`${this.nomeUsuario} é um usuário Inativo`);
         }
     }
 
-    y() {
-        if (this.c === 'admin') {
-            console.log("Admin pode acessar todas as áreas.");
+    verificarPapelUsuario() { 
+        if (this.papelUsuario === 'admin') {
+            console.log(`Admin ${this.nomeUsuario} tem acesso ilimitado.`);
         } else {
-            console.log("Convidado tem acesso limitado.");
+            console.log(`Convidado(a) ${this.nomeUsuario} tem acesso limitado.`);
         }
     }
 }
 
-// Deixe a função clara para que seja possível de compreender que ela verifica o número de usuários com a role de admin
-function t(users) {
-    let c = 0;
+
+function contarAdmin(users) { 
+    let totalAdmin = 0;
     for (let i = 0; i < users.length; i++) {
-        if (users[i].c === 'admin') {
-            c++;
+        if (users[i].papelUsuario === 'admin') {
+            totalAdmin++;
         }
     }
-    return c;
+    return totalAdmin;
 }
 
-// Deixe a função clara para que seja possível de compreender que ela verifica se o número de tentativas de login é excessivo
-function l(a, b) {
-    if (a.h > b) {
-        console.log("Tentativas de login excessivas.");
+function contarGuest(users) { 
+    let totalGuest = 0;
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].papelUsuario === 'guest') {
+            totalGuest++;
+        }
+    }
+    return totalGuest;
+}
+
+
+function checarExcessoLogin(user, limite) { 
+    if (user.tentativasLogin > limite) {
+        console.log(`${user.nomeUsuario} realizou tentativas excessivas de login.`);
     } else {
-        console.log("Tentativas de login sob controle.");
+        console.log(`As tentativas de login de ${user.nomeUsuario} estão sob controle.`);
     }
 }
 
-// Deixe a função clara para que seja possível de compreender que ela verifica qual usuário logou mais recentemente
-function c(u1, u2) {
-    if (u1.e > u2.e) {
-        return u1.a + " logou mais recentemente.";
+
+function checarLoginMaisRecente(u1, u2) { 
+    if (u1.dataUltLogin > u2.dataUltLogin) {
+        return `${u1.nomeUsuario} fez login mais recentemente do que ${u2.nomeUsuario}`;
     } else {
-        return u2.a + " logou mais recentemente.";
+         return `${u2.nomeUsuario} fez login mais recentemente do que ${u1.nomeUsuario}`;
+    }
+       
+}
+
+
+function checarCriacaoUsuario(user) { 
+    const anoEmMilissegundos = 365 * 24 * 60 * 60 * 1000;
+    const hrAtual = new Date();
+    if (hrAtual - user.dataCriacao < anoEmMilissegundos) {
+        console.log(user.nomeUsuario + " é um usuário criado recentemente.");
+    } else {
+        console.log(user.nomeUsuario + " é um usuário antigo.");
     }
 }
 
-// Deixe a função clara para que seja possível de compreender que ela verifica se o usuário foi criado recentemente
-function r(user) {
-    let now = new Date();
-    if (now - user.d < 31536000000) {
-        console.log("Usuário criado recentemente.");
-    } else {
-        console.log("Usuário antigo.");
-    }
-}
-
-let usrs = [
-    new z("Carlos", 25, 'admin', new Date(2023, 1, 15), new Date(2024, 8, 1), true, 100, 2),
-    new z("Ana", 30, 'guest', new Date(2020, 4, 22), new Date(2024, 7, 31), true, 50, 3),
-    new z("José", 29, 'admin', new Date(2022, 10, 5), new Date(2024, 6, 10), false, 200, 5),
-    new z("Maria", 35, 'guest', new Date(2021, 2, 10), new Date(2023, 12, 25), false, 80, 7)
+let usuarios = [
+    new UserProfile("Carlos", 25, 'admin', new Date(2023, 1, 15), new Date(2024, 8, 1), true, 100, 2),
+    new UserProfile("Ana", 30, 'guest', new Date(2020, 4, 22), new Date(2024, 7, 31), true, 50, 3),
+    new UserProfile("José", 29, 'admin', new Date(2022, 10, 5), new Date(2024, 6, 10), false, 200, 5),
+    new UserProfile("Maria", 35, 'guest', new Date(2021, 2, 10), new Date(2023, 12, 25), false, 80, 7),
+    new UserProfile("Lucas", 22, 'guest', new Date(2024, 9, 5), new Date(2024, 6, 10), true, 75, 2) 
 ];
 
-console.log("Usuários admin: " + t(usrs));
+console.log("Usuários admin: " + contarAdmin(usuarios));
+console.log("Usuários guest: " + contarGuest(usuarios));
 
-usrs[0].x();
-usrs[1].y();
+usuarios[0].verificarStatus();
+usuarios[3].verificarStatus();
 
-l(usrs[2], 4);
+usuarios[1].verificarPapelUsuario();
+usuarios[2].verificarPapelUsuario();
 
-console.log(c(usrs[0], usrs[3]));
+checarExcessoLogin(usuarios[2], 4);
+checarExcessoLogin(usuarios[1], 4);
 
-r(usrs[3]);
+console.log(checarLoginMaisRecente(usuarios[0], usuarios[3]));
+
+checarCriacaoUsuario(usuarios[3]);
+checarCriacaoUsuario(usuarios[4]);
