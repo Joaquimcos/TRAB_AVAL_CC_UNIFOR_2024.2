0c1ab8494a496aa99f65e295e16d2948c6d1b416
biiancatorres <biancatorres@edu.unifor.br>
2024-11-12 23:10:47 -0300

    removendo número mágico

--- 02-Segundo nível/01-sample.js
+++ 02-Segundo nível/01-sample.js
@@ -1,5 +1,6 @@
 function calcularDesconto(preco) {
-    return preco - (preco * 0.15);
+    const descontoPadrao = 0.15
+    return preco - (preco * descontoPadrao);
 }
 
 const precoFinal = calcularDesconto(100);
